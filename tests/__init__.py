c3fa8d722241d36af062109aad0bfe9d62dad9ce
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-17 12:28:25 +0000

    Added skeleton tests.

---
+++ tests/__init__.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestLoadCSV(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_load_sheet(self):
+        pass
+
+    def test_load_column(self):
+        pass
+
+    def test_get_column_of_sheet(self):
+        pass
+
+    def test_float_list(self):
+        pass

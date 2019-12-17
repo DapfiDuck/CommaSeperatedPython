b44b3484e25d183c6072b340531789a899c3dede
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-17 12:29:29 +0000

    Added type hints.

--- src/csvloader.py
+++ src/csvloader.py
@@ -1,7 +1,8 @@
 import csv
+from typing import List
 
 
-def load_sheet(url, delimiter=","):
+def load_sheet(url: str, delimiter: str = ","):
     with open(url) as file:
         sheet = csv.reader(file, delimiter=delimiter)
         array = []
@@ -12,15 +13,15 @@ def load_sheet(url, delimiter=","):
     return array
 
 
-def load_column(url, column_nr, delimiter=","):
+def load_column(url, column_nr: int, delimiter=","):
     array = load_sheet(url, delimiter=delimiter)
     column = get_column_of_sheet(array, column_nr)
 
     return column
 
 
-def get_column_of_sheet(sheet, column_nr):
-    column = []
+def get_column_of_sheet(sheet, column_nr: int):
+    column: List = []
 
     for row in sheet:
         column.append(row[column])
@@ -28,8 +29,8 @@ def get_column_of_sheet(sheet, column_nr):
     return column
 
 
-def float_list(to_float):
-    outlist = []
+def float_list(to_float: List) -> List[float]:
+    outlist: List[float] = []
 
     for entry in to_float:
         outlist.append(float(entry))
